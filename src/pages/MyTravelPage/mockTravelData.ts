0c00d6e46c353dc147b72a2b51ba5ad2a683784e
jihongeek <jihongeek@kakao.com>
2025-11-25 18:48:02 +0900

    Feature: 내 여행페이지 UI 구현

---
+++ src/pages/MyTravelPage/mockTravelData.ts
@@ -0,0 +1,47 @@
+export const mockTravels = [
+  {
+    title: '부산여행',
+    id: 1,
+    thumbnail:
+      'https://www.visitbusan.net/uploadImgs/files/cntnts/20220921114743147_wufrotr',
+    startDate: '2025-01-11',
+    endDate: '2025-01-11',
+    location: '부산광역시',
+  },
+  {
+    title: '부산여행',
+    id: 2,
+    thumbnail:
+      'https://i.namu.wiki/i/UXfXVymFAEySTa9__7J6Ch6m8l3OITom9CwxLaYbdibBSjv0V7kl53jVKYDgciZboW0GrqUtsHOhl_r9pz7H_5OoeZN8j2Ikz0kPPBLSf-C4mCIs3XOx9OgzUiXa7hsQ0q1EsbyCuxZjXM48xGRS0Q.webp',
+    startDate: '2025-01-11',
+    endDate: '2025-01-11',
+    location: '부산광역시',
+  },
+  {
+    title: '부산여행',
+    id: 3,
+    thumbnail:
+      'https://i.namu.wiki/i/UXfXVymFAEySTa9__7J6Ch6m8l3OITom9CwxLaYbdibBSjv0V7kl53jVKYDgciZboW0GrqUtsHOhl_r9pz7H_5OoeZN8j2Ikz0kPPBLSf-C4mCIs3XOx9OgzUiXa7hsQ0q1EsbyCuxZjXM48xGRS0Q.webp',
+    startDate: '2025-01-11',
+    endDate: '2025-01-11',
+    location: '부산광역시',
+  },
+  {
+    title: '부산여행',
+    id: 4,
+    thumbnail:
+      'https://i.namu.wiki/i/UXfXVymFAEySTa9__7J6Ch6m8l3OITom9CwxLaYbdibBSjv0V7kl53jVKYDgciZboW0GrqUtsHOhl_r9pz7H_5OoeZN8j2Ikz0kPPBLSf-C4mCIs3XOx9OgzUiXa7hsQ0q1EsbyCuxZjXM48xGRS0Q.webp',
+    startDate: '2025-01-11',
+    endDate: '2025-01-11',
+    location: '부산광역시',
+  },
+  {
+    title: '부산여행',
+    id: 5,
+    thumbnail:
+      'https://i.namu.wiki/i/UXfXVymFAEySTa9__7J6Ch6m8l3OITom9CwxLaYbdibBSjv0V7kl53jVKYDgciZboW0GrqUtsHOhl_r9pz7H_5OoeZN8j2Ikz0kPPBLSf-C4mCIs3XOx9OgzUiXa7hsQ0q1EsbyCuxZjXM48xGRS0Q.webp',
+    startDate: '2025-01-11',
+    endDate: '2025-01-11',
+    location: '부산광역시',
+  },
+];
