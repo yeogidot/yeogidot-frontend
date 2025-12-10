951625fc7b126bf1048ccd3574627fc9d4199c38
jihongeek <jihongeek@kakao.com>
2025-12-10 09:11:53 +0900

    Feature: 파일 선택 버튼 구현

---
+++ src/components/Buttons/FileSelectButton/FileSelectButton.tsx
@@ -0,0 +1,26 @@
+import type { InputHTMLAttributes } from 'react';
+import classes from './FileSelectButton.module.css';
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+export default function FileSelectButton({
+  onChange,
+  children,
+  id = 'select-photo',
+  accept = 'image/*',
+  multiple = true,
+  ...props
+}: Props) {
+  return (
+    <span className={classes.container} aria-label="사진 업로드 버튼">
+      <label htmlFor={id}>{children}</label>
+      <input
+        type="file"
+        accept={accept}
+        id={id}
+        multiple={multiple}
+        onChange={onChange}
+        className={classes.fileInput}
+        {...props}
+      />
+    </span>
+  );
+}
