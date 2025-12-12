5b86bdcd2b833df9208eecdedb557dad41451877
jihongeek <jihongeek@kakao.com>
2025-12-12 16:00:49 +0900

    Refactor: EXIF 태그를 찾을 수 없을 경우 `null` 리턴하게 수정

--- src/utils/exif.ts
+++ src/utils/exif.ts
@@ -7,6 +7,7 @@ export async function getGPSCoordinates(photoFile: File) {
       longitude: Number(tags['GPSLongitude'].description),
     };
   }
+  return null;
 }
 export async function getCreatedDateTime(photoFile: File) {
   const tags = await ExifReader.load(photoFile);
@@ -15,4 +16,5 @@ export async function getCreatedDateTime(photoFile: File) {
       tags['DateTimeOriginal'].description.split(' ');
     return `${dateString.replaceAll(':', '-')}T${timeString}+09:00`;
   }
+  return null;
 }
