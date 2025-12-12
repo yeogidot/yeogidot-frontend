965213c9e2009a438f83690ad31ee227a77ae711
jihongeek <jihongeek@kakao.com>
2025-12-12 20:12:29 +0900

    Feature: 사진 데이터 타입 추가

---
+++ src/types/photo.type.ts
@@ -0,0 +1,13 @@
+export interface PhotoData {
+  url: string;
+  size: number;
+  name: string;
+  warning?: boolean;
+}
+export interface DatedPhotoData extends PhotoData {
+  date: string | null;
+}
+
+export interface FullPhotoData extends DatedPhotoData {
+  GPSCoordinates: { latitude: number; longitude: number } | null;
+}
