8f7093302ffae01ff0d3b15d0fb49bfdb13a3eff
jihongeek <jihongeek@kakao.com>
2026-01-01 20:25:55 +0900

    refactor: props에 기본 버튼 속성 추가 및 추가 스타일 적용 가능하게 수정

--- src/components/Buttons/BackButton/GrayBackButton/GrayBackButton.tsx
+++ src/components/Buttons/BackButton/GrayBackButton/GrayBackButton.tsx
@@ -1,15 +1,16 @@
 import classes from './GrayBackButton.module.css';
 import backIcon from '../../../../assets/icons/back-white.svg';
+import type { ButtonHTMLAttributes } from 'react';
 
 type BackButtonProps = {
   onClick?: () => void;
-};
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function BackButton({ onClick }: BackButtonProps) {
+export default function BackButton({ onClick, className }: BackButtonProps) {
   return (
-    <button 
+    <button
       type="button"
-      className={`${classes.grayIconWrapper} ${classes.mapBackButton}`}
+      className={`${classes.grayIconWrapper} ${classes.mapBackButton} ${className}`}
       onClick={onClick}
     >
       <img src={backIcon} alt="Back" className={classes.icon} />
