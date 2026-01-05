a8cfee1def0e27733529ce3ca2a138b6eebe386e
jihongeek <jihongeek@kakao.com>
2026-01-05 16:50:28 +0900

    chore: 전체 사진 화면 페이지 이름 변경

---
+++ src/pages/NewTravelPhotoPage/NewTravelPhotoPage.tsx
@@ -0,0 +1,40 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import classes from './NewTravelPhotoPage.module.css';
+import type { DatedPhotoData } from 'src/types/photo.type';
+import WarningIcon from '@assets/icons/warning.svg';
+import FullPhotoLayout from '@components/FullPhotoLayout/FullPhotoLayout';
+import GrayBackButton from '@components/Buttons/BackButton/GrayBackButton/GrayBackButton';
+import { convertISOToKorean } from '@utils/date';
+import CircularIconButton from '@components/CircularIconButton/CircularIconButton';
+import CalendarIcon from '@assets/icons/datetime-white.svg';
+import BinIcon from '@assets/icons/trash-can.svg';
+export default function FullPhotoPage() {
+  const photo: DatedPhotoData = useLocation().state;
+  const navigate = useNavigate();
+  return (
+    <FullPhotoLayout photo={photo} imageWidthDefaultPercent={100}>
+      <header className={classes.header}>
+        <GrayBackButton
+          onClick={() => navigate(-1)}
+          className={classes.backButton}
+        />
+        <div className={classes.photoInfo}>
+          {photo.date ? (
+            <h2>{convertISOToKorean(photo.date)}</h2>
+          ) : (
+            <h2>날짜 정보 없음</h2>
+          )}
+          {photo.warning && (
+            <h2 className={classes.warning}>
+              <img src={WarningIcon} alt="경고 아이콘" /> 위치 정보 없음
+            </h2>
+          )}
+        </div>
+      </header>
+      <footer className={classes.footer}>
+        <CircularIconButton icon={CalendarIcon} />
+        <CircularIconButton icon={BinIcon} />
+      </footer>
+    </FullPhotoLayout>
+  );
+}
