d58e7f412737a00541c2a8763985af1060c7a76d
jihongeek <jihongeek@kakao.com>
2026-01-09 22:39:13 +0900

    feat: 새 여행 타입 설정

--- src/types/travel.type.ts
+++ src/types/travel.type.ts
@@ -1,5 +1,11 @@
-export interface TravelInfo {
+import type { FullPhotoData } from './photo.type';
+
+export interface NewTravelInfo {
   title: string;
+  photos: FullPhotoData[];
+  thumbnailPhotoId: number;
+}
+export interface TravelInfo extends NewTravelInfo {
   id: number;
   thumbnail: string;
   startDate: string;
