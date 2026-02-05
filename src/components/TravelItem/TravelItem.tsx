3bea861328c376a5ce28ae228092cfecc46b8f94
jihongeek <jihongeek@kakao.com>
2026-02-05 10:20:05 +0900

    refactor: 백엔드 API에 맞게 여행 정보 타입 수정 및 적용

--- src/components/TravelItem/TravelItem.tsx
+++ src/components/TravelItem/TravelItem.tsx
@@ -1,7 +1,7 @@
 import classes from './TravelItem.module.css';
 import { Link } from 'react-router-dom';
 import EditIcon from '@assets/icons/edit.svg?react';
-import type { TravelInfo } from '@types/travel.type';
+import type { TravelInfo } from 'src/types/travel.type';
 interface Props {
   travel: TravelInfo;
 }
@@ -9,20 +9,20 @@ interface Props {
 export default function TravelItem({ travel }: Props) {
   return (
     <div className={classes.itemContainer}>
-      <Link to={`/travel/${travel.id}`} className={classes.thumbnailLink}>
+      <Link to={`/travel/${travel.travelId}`} className={classes.thumbnailLink}>
         <img
-          src={travel.thumbnail}
+          src={travel.representativeImageUrl}
           alt={`여행 ${travel.title}의 대표이미지`}
           className={classes.thumbnail}
           draggable={false}
         />
       </Link>
-      <Link to={`/travel/${travel.id}`} className={classes.infoContainer}>
+      <Link to={`/travel/${travel.travelId}`} className={classes.infoContainer}>
         <span className={classes.title}>{travel.title}</span> <br />
         {travel.startDate} ~ {travel.endDate} <br />
-        {travel.location}
+        {travel.trvRegion}
       </Link>
-      <Link to={`/edit/${travel.id}`} className={classes.editButton}>
+      <Link to={`/edit/${travel.travelId}`} className={classes.editButton}>
         <EditIcon />
       </Link>
     </div>
