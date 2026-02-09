dbdcf2742da0d4457a538c5d24853de49ffcd914
jihongeek <jihongeek@kakao.com>
2026-02-09 17:35:52 +0900

    feat: 내 여행 페이지 API 연동, 로딩 및 오류 처리

--- src/pages/MyTravelPage/MyTravelPage.tsx
+++ src/pages/MyTravelPage/MyTravelPage.tsx
@@ -1,18 +1,48 @@
 import classes from './MyTravelPage.module.css';
 import NavigationBar from '@components/NavigationBar/NavigationBar';
 import TravelList from '@components/TravelList/TravelList';
+import { useApi } from '@hooks/api';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { mockTravels } from './mockTravelData';
+import { useNavigate } from 'react-router-dom';
+import { travelService } from 'src/apis/services/travel';
 export default function MyTravelPage() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('accessToken');
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    }
+    if (token) {
+      request(token).then(() => {
+        if (status === 403) {
+          navigate('/login');
+        }
+      });
+    }
+  }, [token]);
+  const { data, error, request, loading, status } = useApi(
+    travelService.getTravels
+  );
   return (
     <>
       <header className={classes.header}>
         <h1 className={classes.headerText}>내 여행 목록</h1>
-        <Link to="/login" className={classes.logoutLink}>
+        <Link
+          to="/login"
+          onClick={() => localStorage.removeItem('accessToken')}
+          className={classes.logoutLink}
+        >
           로그아웃
         </Link>
       </header>
-      <TravelList travels={mockTravels} />
+      {error ? error : ''}
+      {loading || !data ? (
+        <div className={classes.loadingText}>불러오는 중...</div>
+      ) : (
+        <TravelList travels={data} />
+      )}
       <NavigationBar nowTab="my-travel" />
     </>
   );
