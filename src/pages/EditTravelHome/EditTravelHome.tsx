7bfaec45ea92223f88242d4eaf490c40d6812777
jihongeek <jihongeek@kakao.com>
2026-02-25 14:12:45 +0900

    feat: 기존 여행 불러오기 구현

--- src/pages/EditTravelHome/EditTravelHome.tsx
+++ src/pages/EditTravelHome/EditTravelHome.tsx
@@ -6,10 +6,9 @@ import classes from './EditTravelHome.module.css';
 import DatePhotoGrid from '@components/DatePhotoGrid/DatePhotoGrid';
 import { useNavigate } from 'react-router-dom';
 import { getCreatedDateTime, getGPSCoordinates } from 'src/utils/exif';
-import { useTravel } from '@hooks/travel';
+import { useEditTravel } from '@hooks/travel';
 import { dateCompare } from '@utils/date';
 import { useState } from 'react';
-
 const checkFileExtension = (file: File, extensions: string[]) => {
   const fileExtension = file.name.split('.').pop()?.toLowerCase();
   if (fileExtension === undefined) {
@@ -19,7 +18,7 @@ const checkFileExtension = (file: File, extensions: string[]) => {
 };
 
 export default function EditTravelHome() {
-  const [travel, setTravel] = useTravel();
+  const { travel, setTravel, loading, error } = useEditTravel();
   const navigate = useNavigate();
 
   const [titleErrorText, setTitleErrorText] = useState('');
@@ -116,42 +115,48 @@ export default function EditTravelHome() {
         </button>
         <h1 className={classes.headerText}>여행 수정</h1>
       </header>
-      <section className={classes.inputSection}>
-        <h3 className={classes.inputHeader}>여행 이름</h3>
-        <input
-          className={`${classes.textInput} ${titleErrorText !== '' ? classes.textInputError : ''}`}
-          onChange={handleTravelTitleInputChange}
-          value={travel.title}
-          type="text"
-          id="travel-name"
-          autoComplete="off"
-        />
-        {titleErrorText !== '' && (
-          <p className={classes.titleErrorHelperText}>{titleErrorText}</p>
-        )}
-      </section>
-      <section className={classes.inputSection}>
-        <h3 className={classes.inputHeader}>여행 사진</h3>
-        <FileSelectButton
-          className={classes.photoSelectButton}
-          onChange={handleFileChange}
-        >
-          사진 업로드
-        </FileSelectButton>
-        {photoErrorText !== '' && (
-          <p className={classes.photoErrorHelperText}>{photoErrorText}</p>
-        )}
-      </section>
-      <p className={classes.uploadDescription}>
-        사진 위치 정보가 없으면 위치가 표시 되지 않습니다.
-        <br />
-        (카카오톡 사진 전송시, 원본 사진 요망)
-      </p>
+      {loading ? (
+        <p className={classes.loadingText}>여행 정보를 불러오는 중입니다...</p>
+      ) : (
+        <>
+          <section className={classes.inputSection}>
+            <h3 className={classes.inputHeader}>여행 이름</h3>
+            <input
+              className={`${classes.textInput} ${titleErrorText !== '' ? classes.textInputError : ''}`}
+              onChange={handleTravelTitleInputChange}
+              value={travel ? travel.title : ''}
+              type="text"
+              id="travel-name"
+              autoComplete="off"
+            />
+            {titleErrorText !== '' && (
+              <p className={classes.titleErrorHelperText}>{titleErrorText}</p>
+            )}
+          </section>
+          <section className={classes.inputSection}>
+            <h3 className={classes.inputHeader}>여행 사진</h3>
+            <FileSelectButton
+              className={classes.photoSelectButton}
+              onChange={handleFileChange}
+            >
+              사진 업로드
+            </FileSelectButton>
+            {photoErrorText !== '' && (
+              <p className={classes.photoErrorHelperText}>{photoErrorText}</p>
+            )}
+          </section>
+          <p className={classes.uploadDescription}>
+            사진 위치 정보가 없으면 위치가 표시 되지 않습니다.
+            <br />
+            (카카오톡 사진 전송시, 원본 사진 요망)
+          </p>
 
-      <DatePhotoGrid photos={travel.photos} />
-      <Button className={classes.button} onClick={handleClickNextButton}>
-        다음
-      </Button>
+          <DatePhotoGrid photos={travel ? travel.photos : []} />
+          <Button className={classes.button} onClick={handleClickNextButton}>
+            다음
+          </Button>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/EditTravelPage/EditTravelPage.tsx
+++ src/pages/EditTravelPage/EditTravelPage.tsx
@@ -1,14 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
-import type { FullTravel, NewTravelInfo } from 'src/types/travel.type';
-import { mockTravel } from './mockTravel';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import type { NewTravelInfo, FullTravel } from 'src/types/travel.type';
+import { travelService } from 'src/apis/services/travel';
 
-const mockTravelData: FullTravel = mockTravel;
-
-export default function EditTravelPage() {
-  const [travelData, setTravelData] = useState<NewTravelInfo>({
-    title: mockTravelData.title,
-    photos: mockTravelData.days
+import { useApi } from '@hooks/api';
+const convertTravelToTravelInfo = (travel: FullTravel) => {
+  return {
+    title: travel.title,
+    photos: travel.days
       .map(({ photos }) => {
         return photos;
       })
@@ -29,7 +28,29 @@ export default function EditTravelPage() {
           file: new File([], ''),
         };
       }),
-    thumbnailPhotoId: mockTravelData.representativePhotoId,
-  });
-  return <Outlet context={[travelData, setTravelData]} />;
+    thumbnailPhotoId: travel.representativePhotoId,
+  };
+};
+export default function EditTravelPage() {
+  const { state } = useLocation();
+  const token = localStorage.getItem('accessToken');
+  const [travel, setTravel] = useState<NewTravelInfo>();
+  const { data, error, request, loading } = useApi(travelService.getTravel);
+
+  useEffect(() => {
+    if (token && !data) {
+      request(state.travelId, token);
+    }
+  }, [token, request, data, state]);
+
+  useEffect(() => {
+    if (error) {
+      alert(error);
+      return;
+    }
+    if (data && !travel) {
+      setTravel(convertTravelToTravelInfo(data));
+    }
+  }, [data, error, travel, convertTravelToTravelInfo]);
+  return <Outlet context={{ travel, setTravel, loading, error }} />;
 }
