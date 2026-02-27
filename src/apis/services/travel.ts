e66be1d4c68f4d2dc6420a91c49f0bf03d63c40b
jihongeek <jihongeek@kakao.com>
2026-02-27 14:02:12 +0900

    refactor: 여행수정 API service 수정

--- src/apis/services/travel.ts
+++ src/apis/services/travel.ts
@@ -7,6 +7,7 @@ import type {
 } from 'src/types/travel.type';
 import { http } from '../http';
 import { photoService } from './photo';
+import type { FullPhotoData } from 'src/types/photo.type';
 
 export const travelService = {
   getTravel: (id: number, token: string) => {
@@ -64,15 +65,13 @@ export const travelService = {
     if (uploadedRepresentativePhoto === undefined) {
       throw new Error('업로드 된 대표 이미지를 알 수 없습니다.');
     }
-    const photoIdArray = uploadedData.data.uploadedPhotos.map(
-      ({ photoId }) => photoId
-    );
+    const photoIdArray = uploadedData.data.uploadedPhotos.map(({ id }) => id);
     return http.post<number>(
       `/api/travels`,
       {
         title: travel.title,
         photoIds: photoIdArray,
-        representativePhotoId: uploadedRepresentativePhoto.photoId,
+        representativePhotoId: uploadedRepresentativePhoto.id,
       },
       token
     );
@@ -89,14 +88,79 @@ export const travelService = {
   deleteTravel: (id: number, token: string) => {
     return http.delete(`/api/travels/${id}`, token);
   },
-  updateTravelInfo: (
+  updateTravelInfo: async (
     travelId: number,
-    newTravelInfo: Partial<FullTravel>,
+    travel: NewTravelInfo,
     token: string
   ) => {
+    const newPhotos = travel.photos.filter(
+      ({ url }) => new URL(url).protocol === 'blob:'
+    );
+    const oldPhotos = travel.photos.filter(
+      ({ url }) => new URL(url).protocol !== 'blob:'
+    );
+    if (newPhotos.length === 0) {
+      return http.patch<{ status: number; message: string }>(
+        `/api/travels/${travelId}`,
+        {
+          title: travel.title,
+          photoIds: travel.photos.map(({ id }) => id),
+          representativePhotoId: travel.thumbnailPhotoId,
+        },
+        token
+      );
+    }
+    const metadataArray = newPhotos.map(({ date, GPSCoordinates, file }) => {
+      return {
+        takenAt: date,
+        longitude: GPSCoordinates?.longitude ?? null,
+        latitude: GPSCoordinates?.latitude ?? null,
+        originalName: file.name,
+      };
+    });
+    // 현재는 브라우저에서 만든 ID로 대표 이미지를 지정하고 있음, 근데 여행을 수정할 때는 업로드한 사진의 ID 필요
+    // 사진 파일의 이름과 찍은 시간이 같으면 같은 대표 이미지로 취급하여 ID 선택
+    if (travel.thumbnailPhotoId === null) {
+      throw new Error('대표 이미지 정보가 없습니다.');
+    }
+    const uploadedData = await photoService.uploadPhoto(
+      newPhotos.map(({ file }) => file),
+      metadataArray,
+      token
+    );
+    if (uploadedData.data === undefined) {
+      throw new Error('업로드한 사진 데이터를 불러올 수 없습니다.');
+    }
+    const localRepresentativePhoto = travel.photos.find(
+      photo => photo.id === travel.thumbnailPhotoId
+    );
+    const uploadedRepresentativePhoto = uploadedData.data.uploadedPhotos.find(
+      photo =>
+        photo.takenAt === localRepresentativePhoto?.date &&
+        photo.originalName === localRepresentativePhoto?.file.name
+    );
+
+    if (
+      uploadedRepresentativePhoto === undefined &&
+      newPhotos.includes(localRepresentativePhoto as FullPhotoData)
+    ) {
+      throw new Error('업로드 된 대표 이미지를 알 수 없습니다.');
+    }
+    const photoIdArray = [
+      ...oldPhotos.map(({ id }) => id),
+      ...uploadedData.data.uploadedPhotos.map(({ id }) => id),
+    ];
+    console.log(photoIdArray);
+
     return http.patch<{ status: number; message: string }>(
       `/api/travels/${travelId}`,
-      newTravelInfo,
+      {
+        title: travel.title,
+        photoIds: photoIdArray,
+        representativePhotoId: uploadedRepresentativePhoto
+          ? uploadedRepresentativePhoto.id
+          : localRepresentativePhoto?.id,
+      },
       token
     );
   },
