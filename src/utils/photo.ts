8d0fa96d3d253d7e6eb3224da0691d52f397898e
jihongeek <jihongeek@kakao.com>
2026-03-14 10:31:54 +0900

    refactor: 메모리 누수 처리 및 에러 핸들링 로직 수정

--- src/utils/photo.ts
+++ src/utils/photo.ts
@@ -2,25 +2,37 @@ export async function photoFileToWebp(photoFile: File) {
   return new Promise<Blob>((resolve, reject) => {
     const imageURL = URL.createObjectURL(photoFile);
     const image = new Image();
-    image.src = imageURL;
+    image.onerror = () => {
+      reject(
+        new Error('이미지 변환에 사용될 이미지를 불러오기 실패하였습니다.')
+      );
+      URL.revokeObjectURL(imageURL);
+    };
     image.onload = () => {
       const canvas = document.createElement('canvas');
       canvas.width = image.width;
       canvas.height = image.height;
       const context = canvas.getContext('2d');
+      if (!context) {
+        reject(
+          new Error('이미지 변환에 필요한 Canvas Context를 찾을 수 없습니다.')
+        );
+        return;
+      }
       context?.drawImage(image, 0, 0);
       canvas.toBlob(
         blob => {
           if (blob) {
             resolve(blob);
           } else {
-            throw Error('이미지 변환 실패');
+            reject(new Error('이미지 변환 실패'));
           }
         },
         'image/webp',
         0.7
       );
-      image.onerror = reject;
+      URL.revokeObjectURL(imageURL);
     };
+    image.src = imageURL;
   });
 }
