58a422da49edc507b39c0261e56ea066a284c7bd
jihongeek <jihongeek@kakao.com>
2026-03-24 09:08:42 +0900

    chore: 미사용 onClick Props 제거

--- src/components/Map/PhotoMarker.tsx
+++ src/components/Map/PhotoMarker.tsx
@@ -7,18 +7,14 @@ interface PhotoMarkerProps {
   onClick?: () => void;
 }
 
-export default function PhotoMarker({
-  photoUrl,
-  position,
-  onClick,
-}: PhotoMarkerProps) {
+export default function PhotoMarker({ photoUrl, position }: PhotoMarkerProps) {
   const icon = L.icon({
     iconUrl: photoUrl,
     className: classes.photoMarker,
     iconSize: [50, 50],
     iconAnchor: [25, 25], // 마커 중앙 기준
   });
-
+  console.log('sd');
   return (
     <Marker
       position={position}
