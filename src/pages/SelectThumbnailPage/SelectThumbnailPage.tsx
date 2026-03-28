0848c7fd261274ef9cf29174a5f5b91a95627fc7
jihongeek <jihongeek@kakao.com>
2026-03-28 23:34:43 +0900

    fix: 화면 내 공간 부족으로 인한 로딩 메시지 삭제

--- src/pages/SelectThumbnailPage/SelectThumbnailPage.tsx
+++ src/pages/SelectThumbnailPage/SelectThumbnailPage.tsx
@@ -94,16 +94,6 @@ export default function SelectThumbnailPage() {
       >
         {loading ? '여행 추가 중...' : '여행 추가'}
       </Button>
-      <p className={classes.loadingMessage} role="status" aria-live="polite">
-        {loading ? (
-          <>
-            여행을 추가하고 있어요. <br /> 완료되면 자동으로 여행 페이지로
-            이동해요.
-          </>
-        ) : (
-          ''
-        )}
-      </p>
       {modalElement}
     </div>
   );
